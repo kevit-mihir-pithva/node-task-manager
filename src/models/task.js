fe24fbb0b74d79eaf3ba50b2370aa887780a72f9
Mihir Pithva <mihir.pithva@kevit.io>
2023-09-26 10:38:19 +0530

    extra tests added and one change in task model

--- src/models/task.js
+++ src/models/task.js
@@ -4,7 +4,8 @@ const taskSchema = new mongoose.Schema({
     description:{
         type:String,
         required:true,
-        trim:true
+        trim:true,
+        minlength:5
     },
     completed:{
         type:Boolean,
